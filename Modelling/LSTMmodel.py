90986ea4881a5d1b5ab39becc19cc31351930dfa
JohanOelgaard <johan.oelgaard@gmail.com>
2024-12-15 13:27:33 +0100

    Running the LSTM model again with the expected final parameters

--- Modelling/LSTMmodel.py
+++ Modelling/LSTMmodel.py
@@ -1,13 +1,8 @@
-import pandas as pd
-import numpy as np
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-import numpy as np
+
 
 # create data class
 class dataset(Dataset):
